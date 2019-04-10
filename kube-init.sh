75f5feec70f7d960d139694a5786c64bd1634790
sacred <sacred02@163.com>
2019-04-10 18:15:06 +0800

    step2. kube-init.sh
    step2. kube-init.sh

---
+++ kube-init.sh
@@ -0,0 +1,8 @@
+#kube-init.sh (ONLY MASTER)
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
